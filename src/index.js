0ae0cc330fed25b98d9a9d4d1f528b0fb8f303df
Diego Perpétuo <104659648+diegoperpetuo@users.noreply.github.com>
2025-05-12 01:24:37 +0000

    Update index.js with serverless function

--- src/index.js
+++ src/index.js
@@ -1,52 +1,30 @@
-require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
-const serverless = require("serverless-http");
+const serverless = require('serverless-http');
+const morgan = require('morgan');
 const moviesRoutes = require('./routes/movie');
 const authRoutes = require('./routes/authRoutes');
-const morgan = require('morgan');
 const connectDatabase = require('./database/connection');
 
-const app = express();
+require('dotenv').config();
+connectDatabase(); // conecta ao Mongo
 
-require("dotenv").config();
-require("../src/database/connection")();
+const app = express();
 
 // Middlewares globais
 app.use(express.json());
 app.use(morgan('dev'));
 
-connectDatabase();
-
-module.exports.handler = serverless(app);
-
-
+// Rotas
 app.get('/', (req, res) => {
-  const resp = { message: 'Hello World!' };
-  res.json(resp).status(200);
+  res.status(200).json({ message: 'Hello from Vercel!' });
 });
 
-// Rotas públicas
 app.use('/auth', authRoutes);
-
-// Rotas protegidas
 app.use('/movies', moviesRoutes);
 
-// Tratamento de rotas inexistentes
 app.use((req, res) => {
   res.status(404).json({ error: 'Rota não encontrada' });
 });
 
-// Conexão com o MongoDB e inicialização do servidor
-const PORT = process.env.PORT || 3000;
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => {
-  console.log('MongoDB conectado');
-  app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
-})
-.catch(err => {
-  console.error('Erro ao conectar ao MongoDB:', err);
-});
+module.exports = app;
+module.exports.handler = serverless(app);
