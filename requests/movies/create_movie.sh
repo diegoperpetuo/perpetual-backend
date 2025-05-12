4a3aae5fef8421886e8074459b0866b4513087fa
Diego Perpétuo <104659648+diegoperpetuo@users.noreply.github.com>
2025-05-12 01:51:01 +0000

    Fix host to create movie script

--- requests/movies/create_movie.sh
+++ requests/movies/create_movie.sh
@@ -1,4 +1,4 @@
-curl -X POST https://movies-crud-three.vercel.app/movies \
+curl -X POST http://localhost:5000/movies \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTViMjcyNzVmNjcwNDFlZGRjZTJkMCIsImlhdCI6MTc0NzAxNDA2MCwiZXhwIjoxNzQ3MDE3NjYwfQ.mhJN7Urt2vggLCGHkARRtqxpZzHCT7KydNTHCU-R-sQ" \
 -d '{
