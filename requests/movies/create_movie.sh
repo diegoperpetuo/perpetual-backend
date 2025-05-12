dad68cbda2af0d0cf41a5cbe388dec6b801ec6e0
Diego Perpétuo <104659648+diegoperpetuo@users.noreply.github.com>
2025-05-12 01:54:38 +0000

    Test with vercel path

--- requests/movies/create_movie.sh
+++ requests/movies/create_movie.sh
@@ -2,8 +2,8 @@ curl -X POST http://localhost:5000/movies \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTViMjcyNzVmNjcwNDFlZGRjZTJkMCIsImlhdCI6MTc0NzAxNDA2MCwiZXhwIjoxNzQ3MDE3NjYwfQ.mhJN7Urt2vggLCGHkARRtqxpZzHCT7KydNTHCU-R-sQ" \
 -d '{
-  "title": "Karate Kid",
-  "genre": "Fight",
-  "releaseYear": 1984,
-  "rating": 9.1
+  "title": "Karate Kid: Legends",
+  "genre": "Action",
+  "releaseYear": 2025,
+  "rating": 7.0
 }'
--- requests/movies/get_movies.sh
+++ requests/movies/get_movies.sh
@@ -1,2 +1,2 @@
-curl -X GET http://localhost:5000/movies \
+curl -X GET https://movies-crud-three.vercel.app/api/movies \
 -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTViMjcyNzVmNjcwNDFlZGRjZTJkMCIsImlhdCI6MTc0NzAxNDA2MCwiZXhwIjoxNzQ3MDE3NjYwfQ.mhJN7Urt2vggLCGHkARRtqxpZzHCT7KydNTHCU-R-sQ"
